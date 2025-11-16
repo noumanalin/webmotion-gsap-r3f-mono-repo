c3615395b79a3dc6c53ded3538cead69adbdd64d
naumanalin <naumanalin865@gmail.com>
2025-11-16 22:18:20 +0500

    Spylt - Hero Banner Completed

--- Spylt-GSAP-Awwwarded-website/src/App.jsx
+++ Spylt-GSAP-Awwwarded-website/src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar"
+import Hero from "./components/sections/Hero"
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother); 
 
 const App = () => {
   return (
     <>
       <Navbar />
       <main>
+        <Hero/>
+        <div className="h-screen w-screen"></div>
         main
       </main>
     </>
